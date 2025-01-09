0472805f33db6bb9ceb35ac74b7eae66c5a95449
Oskar Gester <gester.oskar@gmail.com>
2025-01-09 11:16:15 +0100

    comments in availability repository

--- src/main/java/health/care/booking/respository/AvailabilityRepository.java
+++ src/main/java/health/care/booking/respository/AvailabilityRepository.java
@@ -4,8 +4,8 @@ import health.care.booking.models.Availability;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface AvailabilityRepository extends MongoRepository<Availability, String> {
-    // skall den extenda Mongrepository som i secondroll?
-    // med <Availability, String>
 
-    //göra en findByCaregiverId(userId)?
+    //create findByCaregiverId(userId) method?
+
+    //create find
 }
